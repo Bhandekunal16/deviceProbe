85f20f319b210503c6c4858ee8104a4aa085d9c7
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-10 17:43:58 +0530

    "Refactor: Modified import statement to include 'global' module and replaced hardcoded Cypher query with a dynamic one from the 'global' module."

--- brain.js
+++ brain.js
@@ -1,4 +1,4 @@
-const [App, express, cors, bodyParser, useragent, neo4j, Logger] = [
+const [App, express, cors, bodyParser, useragent, neo4j, Logger, global] = [
   require("./app"),
   require("express"),
   require("cors"),
@@ -6,6 +6,7 @@ const [App, express, cors, bodyParser, useragent, neo4j, Logger] = [
   require("useragent"),
   require("neo4j-driver"),
   require("robotic.js/src/interface/Logger"),
+  require("./global/global"),
 ];
 
 const app = express();
@@ -53,38 +54,7 @@ app.post("/", async (req, res) => {
   ];
   const agent = useragent.parse(userAgentString);
   const deviceName = agent.device.toString();
-  const query = `
-    MERGE (p:Person { deviceName: $deviceName, country: $country,
-      country_name: $country_name,
-      country_code: $country_code,
-      country_code_iso3: $country_code_iso3,
-      country_capital: $country_capital,
-      country_tld: $country_tld,
-      continent_code: $continent_code,
-      in_eu: false,
-      postal: $postal,
-      latitude: $latitude,
-      longitude: $longitude,
-      timezone: $timezone,
-      utc_offset: $utc_offset,
-      country_calling_code: $country_calling_code,
-      currency: $currency,
-      currency_name: $currency_name,
-      languages: $languages,
-      country_area: $country_area,
-      country_population: $country_population,
-      asn: $asn,
-      org: $org,
-      ip: $ip,
-      network: $network, 
-      version: $version, 
-      city: $city, 
-      region: $region, 
-      region_code: $region_code,
-      deviceLatitude: $deviceLatitude,
-      deviceLongitude: $deviceLongitude 
-    }) 
-    RETURN p`;
+  const query = new global().query;
 
   const params = {
     ip: obj.ip,
