3312771be1395d361923aabebb0375b2e0854084
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 09:18:51 +0530

    "Change 'connection' property from instance to static in environment class"

--- env/environment.js
+++ env/environment.js
@@ -1,6 +1,6 @@
 class environment {
   /** @type {string}*/
-  connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
+  static connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
 
   /** @type {string}*/
   name = "neo4j";
