c81330758469add2b3fb3291f31acc816eb3979b
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 09:27:29 +0530

    "Removed 'static' keyword from environment class properties"

--- env/environment.js
+++ env/environment.js
@@ -1,12 +1,12 @@
 class environment {
   /** @type {string}*/
-  static connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
+   connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
 
   /** @type {string}*/
-  static name = "neo4j";
+   name = "neo4j";
 
   /** @type {string}*/
-  static password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk";
+   password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk";
 }
 
 module.exports = environment;
