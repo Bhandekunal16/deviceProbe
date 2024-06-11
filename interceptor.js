63ef6048f8494c632ba9914361ebf9f91f5d3aae
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 17:24:34 +0530

    done

--- interceptor.js
+++ interceptor.js
@@ -1,30 +1,13 @@
-const [db, Logger] = [
-  require("roboticdb/src/brain"),
-  require("robotic.js/src/interface/Logger"),
-];
+const Logger = require("robotic.js/src/interface/Logger");
 module.exports = function isLogger(req, res, next) {
-  next(
-    req.hostname == "localhost"
-      ? new db().write(
-          {
-            url: req.url,
-            method: req.method,
-            host: req.hostname,
-            body: req.body,
-            hitOn: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
-          },
-          "log"
-        )
-      : logRequest(req)
-  );
-};
+  const array = {
+    url: req.url,
+    method: req.method,
+    host: req.hostname,
+    body: req.body,
+    hitOn: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
+  };
+  const query = Object.entries(array).map(([key, value]) => `${key}: ${value}`);
 
-logRequest = (req) => {
-  new Logger().log(req.url),
-    new Logger().log(req.method),
-    new Logger().log(req.hostname),
-    new Logger().log(req.body),
-    new Logger().log(
-      `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`
-    );
+  next(new Logger().array(query));
 };
