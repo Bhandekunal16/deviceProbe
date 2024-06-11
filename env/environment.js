8929665658980bc8a872d61fe804860042fabd31
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 09:19:12 +0530

    "Changed 'name' and 'password' properties in environment class from instance properties to static properties."

--- env/environment.js
+++ env/environment.js
@@ -3,10 +3,10 @@ class environment {
   static connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
 
   /** @type {string}*/
-  name = "neo4j";
+  static name = "neo4j";
 
   /** @type {string}*/
-  password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk";
+  static password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk";
 }
 
 module.exports = environment;
