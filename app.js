d56a4dd1ad590cd015f51a8240edf4375217d3fe
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 16:20:40 +0530

    "Simplified error handling in OsService class"

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ class OsService {
       const response = await fetch(`https://ipapi.co/${ip}/json/`);
       const info = await response.json();
       if (response.ok) return info.error ? info : info;
-      else
-        throw new Error(
-          `Failed to retrieve location information. Status code: ${response.status}`
-        );
+      else throw new Error(response.status);
     } catch (error) {
       throw new Error(error);
     }
