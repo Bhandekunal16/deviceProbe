bed500bb54c20e36c800620df919fc540da2d786
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 16:20:15 +0530

    "Simplified conditional statement in OsService class"

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ class OsService {
     try {
       const response = await fetch(`https://ipapi.co/${ip}/json/`);
       const info = await response.json();
-      if (response.ok) {
-        return info.error ? info : info;
-      } else {
+      if (response.ok) return info.error ? info : info;
+      else
         throw new Error(
           `Failed to retrieve location information. Status code: ${response.status}`
         );
-      }
     } catch (error) {
       throw new Error(error);
     }
