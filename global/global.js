687b302d36f68eb4ca0b4084bf0842246f56919f
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-11 09:25:49 +0530

    "Extracted object creation into a new method in global.js and updated brain.js to use it"

--- global/global.js
+++ global/global.js
@@ -31,6 +31,41 @@ class Type {
             deviceLongitude: $deviceLongitude 
             }) 
             RETURN p`;
+
+  static method(obj) {
+    return {
+      ip: obj.ip,
+      network: obj.network,
+      version: obj.version,
+      city: obj.city,
+      region: obj.region,
+      region_code: obj.region_code,
+      country: obj.country,
+      country_name: obj.country_name,
+      country_code: obj.country_code,
+      country_code_iso3: obj.country_code_iso3,
+      country_capital: obj.country_capital,
+      country_tld: obj.country_tld,
+      continent_code: obj.continent_code,
+      in_eu: false,
+      postal: obj.postal,
+      latitude: obj.latitude,
+      longitude: obj.longitude,
+      timezone: obj.timezone,
+      utc_offset: obj.utc_offset,
+      country_calling_code: obj.country_calling_code,
+      currency: obj.currency,
+      currency_name: obj.currency_name,
+      languages: obj.languages,
+      country_area: obj.country_area,
+      country_population: obj.country_population,
+      asn: obj.asn,
+      org: obj.org,
+      deviceName: deviceName,
+      deviceLatitude: requestData.deviceLatitude,
+      deviceLongitude: requestData.deviceLongitude,
+    };
+  }
 }
 
 module.exports = Type;
