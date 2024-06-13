ce3d1caff444e999c8feacacf2f6ef1fb3700a1b
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-13 09:34:18 +0530

    Remove type annotations for connection, name, and password properties in environment class.

--- env/environment.js
+++ env/environment.js
@@ -1,12 +1,9 @@
 class environment {
-  /** @type {string}*/
-   connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
+  connection = "neo4j+s://b76e3d84.databases.neo4j.io:7687";
 
-  /** @type {string}*/
-   name = "neo4j";
+  name = "neo4j";
 
-    /** @type {string}*/
-   password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk"
+  password = "kH8WQkwu-vK5bmjUYjJ2oe1kbcBeoZdDeErj9o8woSk";
 }
 
 module.exports = environment;
